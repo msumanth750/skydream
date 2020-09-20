12624d7198a1661fdee6005068a75056e6e9f453
Sumanth <msumanth750@gmail.com>
2020-09-20 11:54:49 +0530

    initial commit

---
+++ dashboard/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+
+import numpy as np
+import random
+
+# Create your views here.
+def right_poles(poles):
+    rhs_poles =[]
+    for p in poles:
+        if p.real>0:
+            rhs_poles.append(p)
+    return rhs_poles
+
+def stability(poles):
+    rhs_poles=right_poles(poles)
+    if len(rhs_poles)==0:
+        return 'Stable'
+    else:
+        return 'Unstable'
+
+
+def index(request):
+    return render(request,'dashboard/index.html')
+
+def register(request):
+    return render(request,'dashboard/register.html')
+
+def login(request):
+    return render(request,'dashboard/login.html')
+
+def contact(request):
+    return render(request,'dashboard/contact.html')
+
+def about(request):
+    return render(request,'dashboard/about.html')
+
+def rootlocus(request):
+    gradients = ['warm-flame-gradient','night-fade-gradient',
+    'spring-warmth-gradient','rainy-ashville-gradient']
+    pole1 =[]
+    pole2 =[]
+    pole3 =[]
+    pole4 =[]
+    pole5 =[]
+    pole6 =[]
+    pole7 =[]
+    for k in np.arange(0,30,0.1):
+        eqn = [1,6,11,6+2*k]
+        roots = np.roots(eqn)
+        pole1.append(roots[0])
+        pole2.append(roots[1])
+        pole3.append(roots[2])
+
+    gradient = random.choice(gradients)
+    context = {
+    'gradient':gradient,
+    "pole1":pole1,
+    "pole2":pole2,
+    "pole3":pole3,
+    }
+    return render(request,'dashboard/rootlocus.html',context)
+
+
+def polezero(request):
+    if (request.method == 'POST'):
+        print('*'*100)
+        num = request.POST.get('num')
+        den = request.POST.get('den')
+        ntf = [int(a) for a in num.split(" ")]
+        dtf = [int(a) for a in den.split(" ")]
+        print(ntf,dtf)
+        zeros = np.roots(ntf)
+        poles = np.roots(dtf)
+        rhs_poles = right_poles(poles)
+        stable = stability(poles)
+        print(zeros,poles)
+        result = {
+            "num" : ntf,
+            "den" : dtf,
+            "poles" :poles,
+            "zeros": zeros,
+            "rhs_poles" : rhs_poles,
+            "stable":stable,
+        }
+        return render(request,'dashboard/polezero.html',result)
+    return render(request,'dashboard/polezero.html')
---
+++ dreamproject/urls.py
@@ -0,0 +1,32 @@
+"""dreamproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from dashboard import views as dashboard
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', dashboard.index,name='index'),
+    path('login/', dashboard.login,name='login'),
+    path('register/', dashboard.register,name='register'),
+    path('contact/', dashboard.contact,name='contact'),
+    path('rootlocus/', dashboard.rootlocus,name='rootlocus'),
+    path('polezero/', dashboard.polezero,name='polezero'),
+    path('about/', dashboard.about,name='about'),
+
+
+
+]
